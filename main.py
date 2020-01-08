e800a5dbe3bbc222fcc2fdda0bc6ee1cb84b0dfc
aroop <aroopnanda@gmail.com>
2020-01-08 01:39:03 -0600

    Added submitted, accepted and acceptance percent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver                   
 from selenium.webdriver.common.keys import Keys
-import time                         
+import time  
+import os            
 from selenium.webdriver.support.ui import Select
 browser = webdriver.Safari()  
 browser.maximize_window()
@@ -13,6 +14,8 @@ time.sleep(5)
 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 select.select_by_visible_text('all')
 links = browser.find_elements_by_tag_name('a')
+file = open("leetcode_data_partial.csv","a")
+file.write("LINK,LIKES,DISLIKES,DIFFICULTY,Accepted,Submitted,Acceptance %\n")
 visit=[]
 c=0
 for i in links:
@@ -23,7 +26,9 @@ for i in links:
 
 data =[]
 errors=[]
+visit.pop(0)
 total = len(visit)
+
 for i,link in enumerate(visit):
     try:
         print("{}/{}".format(i,total),end=" ")
@@ -32,16 +37,24 @@ for i,link in enumerate(visit):
         buttons = browser.find_elements_by_tag_name("button")
         el = browser.find_elements_by_class_name("css-dcmtd5") + browser.find_elements_by_class_name(
             "css-t42afm") + browser.find_elements_by_class_name("css-14oi08n")
-        data.append((link, buttons[1].text, buttons[2].text, el[0].text))
-        print()
+        submitted = browser.find_elements_by_class_name("css-jkjiwi")
+        acc = submitted[0].text.replace(",", "")
+        submitted = submitted[1].text.replace(",", "")
+        accept_percent = round(int(acc) / int(submitted),2)
+        data.append((link, buttons[1].text, buttons[2].text,
+                     el[0].text, acc, submitted, accept_percent))
+        file.write("{},{},{},{},{},{},{}\n".format(link, buttons[1].text, buttons[2].text,
+                                          el[0].text, acc, submitted, accept_percent))
+        print("Visited : {}".format(link))
     except:
-        print("Couldn't visit {}".format(link))
+        print("Error while visiting : {}".format(link))
         errors.append(i)
         pass
-with open("leetcode_data.csv","w") as file:
-    for link,like,dislike,diff in data:
-        file.write("{},{},{},{}\n".format(link, like, dislike, diff))
-
+# with open("leetcode_data.csv","w") as file:
+#     for link, like, dislike, diff, acc, submitted, accept_percent in data:
+#         file.write("{},{},{},{}\n".format(link, like, dislike,
+#                                           diff, acc, submitted, accept_percent))
+os.rename("leetcode_data_partial.csv", "leetcode_data.csv")
 with open("errors.csv", "w") as file:
     for link in errors:
         file.write("{}\n".format(link))
